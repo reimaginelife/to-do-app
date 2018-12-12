96f0ebc9167d0ceab8710b2bd3e700fa0f69de70
Raymond <reimagineus@gmail.com>
2018-12-11 19:29:26 -0800

    Reformat Javascript for State & UI

--- app.js
+++ app.js
@@ -1,61 +1,43 @@
 function onReady() {
-  let toDos = [];
+  const toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
 
-  const addToDoForm = document.getElementById('addToDoForm')
-  const newToDoText = document.getElementById('newToDoText')
-  const toDoList = document.getElementById('toDoList')
-  const deleteToDo = document.getElementById('deleteToDo')
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) {return;}
 
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
 
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
-
-    //get the newToDoText
-    let title = newToDoText.value;
-
-
-    //create a new li
-    let newLi = document.createElement('li');
-
-    //create a new input
-    let checkbox = document.createElement('input')
-
-
-
-
-    //delete button
-    let deleteToDo = document.createElement('button')
-
-    deleteToDo.textContent = "Delete";
-    deleteToDo.className = 'mdl-button mdl-js-button mdl-js-ripple-effect';
-
-    //set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    //set the title
-    newLi.textContent = title;
-
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    //attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //attach delete button to item
-    newLi.appendChild(deleteToDo)
-
-    //empty the input
+    });
     newToDoText.value = '';
 
-    deleteToDo.addEventListener('click', function(event) {
-      toDoList.removeChild(this.parentElement);
-    })
+    renderTheUI();
+  };
 
+  function renderTheUI(){
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li')
+      const checkbox = document.createElement('input')
+      checkbox.type = "checkbox";
 
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener ('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+    console.log (event);
   });
 
-
-
+  renderTheUI();
 }
 
 window.onload = function() {
